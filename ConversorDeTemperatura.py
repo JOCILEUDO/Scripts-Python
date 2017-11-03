d859e179c230f95c47c0736f948e3909265cd67b
Jocileudo Sousa <jocileudosousav@gmail.com>
2017-11-03 12:35:32 -0200

    Programa que coverte a temperatura em Fahrenheit para Celcius.

---
+++ ConversorDeTemperatura.py
@@ -0,0 +1,6 @@
+temperaturaFahrenheit_str = input("Forneça a temperatura em Fahrenheit: ")
+temperaturaFahrenheit = float(temperaturaFahrenheit_str)
+
+temperaturaCelcius = (temperaturaFahrenheit - 32)*5 /9
+
+print("A temperatura em Graus Celcius é: ",temperaturaCelcius,"°")
